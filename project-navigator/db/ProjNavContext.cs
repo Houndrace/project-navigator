3d6768a5e4ddebf76fa8f73293e24a33fed96c86
houndrace <hound322@gmail.com>
2024-03-07 12:20:56 +0300

    Added a configuration file

--- project-navigator/db/ProjNavContext.cs
+++ project-navigator/db/ProjNavContext.cs
@@ -1,17 +1,25 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using project_navigator.models;
 
 namespace project_navigator.db;
 
 public class ProjNavContext : DbContext
 {
+    private readonly IConfiguration _configuration;
+
+    public ProjNavContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
     public DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         base.OnConfiguring(optionsBuilder);
-        // TODO:Спрятать строку в конфиг файл и добавить в гитигнор
-        optionsBuilder.UseSqlServer(
-            "Server=localhost;Database=ProjectNavigator;Trusted_Connection=True;TrustServerCertificate=True;");
+        //TODO:разобраться со строкой в продакшене
+        var conString = _configuration.GetConnectionString("Db");
+        optionsBuilder.UseSqlServer(conString);
     }
 }
