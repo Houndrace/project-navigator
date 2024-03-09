2ad4938eeffc1ddd978290e48b79c2965bea0653
houndrace <hound322@gmail.com>
2024-03-09 22:33:08 +0300

    Added dto for transferring registration data

--- project-navigator/models/User.cs
+++ project-navigator/models/User.cs
@@ -1,5 +1,11 @@
 namespace project_navigator.models;
 
+public class RegistrationDto
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+}
+
 public class User
 {
     public int Id { get; set; }
