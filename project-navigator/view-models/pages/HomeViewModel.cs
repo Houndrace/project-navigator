761f173c01c583974bfe96f4a7f83546bdab8005
houndrace <hound322@gmail.com>
2024-03-10 11:42:40 +0300

    Fixed HomeViewModel.cs

--- project-navigator/view-models/pages/HomeViewModel.cs
+++ project-navigator/view-models/pages/HomeViewModel.cs
@@ -1,8 +1,8 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using project_navigator.services;
+
 namespace project_navigator.view_models.pages;
 
-public class HomeViewModel
-{
-    
 public partial class HomeViewModel : ObservableObject
 {
     private readonly INavService _navService;
